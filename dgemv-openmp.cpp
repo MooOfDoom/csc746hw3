c466763c58358f1434fc8bb558e1795fe7221a15
MooOfDoom <lotharnarins@hotmail.com>
2021-09-20 15:19:23 -0700

    Add openmp VMM

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -14,16 +14,25 @@ const char* dgemv_desc = "OpenMP dgemv.";
 
 void my_dgemv(int n, double* A, double* x, double* y) {
 
-   #pragma omp parallel
-   {
-      int nthreads = omp_get_num_threads();
-      int thread_id = omp_get_thread_num();
-      printf("Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
-   }
+	#pragma omp parallel
+	{
+		int nthreads = omp_get_num_threads();
+		int thread_id = omp_get_thread_num();
+		// printf("Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
+		int total = n/nthreads;
+		int end = total*(thread_id + 1);
+		for (int i = total*thread_id; i < end; ++i)
+		{
+			for (int j = 0; j < n; ++j)
+			{
+				y[i] += A[i*n + j]*x[j];
+			}
+		}
+	}
 
-   // insert your dgemv code here. you may need to create additional parallel regions,
-   // and you may want to comment out the above parallel code block that prints out
-   // nthreads and thread_id so as to not taint your timings
+	// insert your dgemv code here. you may need to create additional parallel regions,
+	// and you may want to comment out the above parallel code block that prints out
+	// nthreads and thread_id so as to not taint your timings
 
 }
 
